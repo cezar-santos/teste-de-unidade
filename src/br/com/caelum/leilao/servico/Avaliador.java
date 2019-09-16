7f3ff8e97dd67beab7516233168bcaeb4731d196
cezar-santos <cezar-augusto1@hotmail.com>
2019-09-16 17:13:55 -0300

    Meu primeiro comite

---
+++ src/br/com/caelum/leilao/servico/Avaliador.java
@@ -0,0 +1,35 @@
+package br.com.caelum.leilao.servico;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+
+public class Avaliador {
+	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
+    private double menorDeTodos = Double.POSITIVE_INFINITY;
+    private double media = 0;
+    private double total = 0;
+    
+    public void avalia(Leilao leilao) {
+
+    	
+        for(Lance lance : leilao.getLances()) {
+            if(lance.getValor() > maiorDeTodos) maiorDeTodos = lance.getValor();
+            if(lance.getValor() < menorDeTodos) menorDeTodos = lance.getValor();
+            total += lance.getValor();
+        }
+        if(total == 0) {
+            media = 0;
+            return;
+        }
+        media = total / leilao.getLances().size();
+    }
+
+    public double getMaiorLance() { return maiorDeTodos; }
+   
+    public double getMenorLance() { return menorDeTodos; }
+    
+    public double getTotalLance() {return total;}
+    
+    public double getMediaLance( Leilao leilao) {return media;}
+}
+	
